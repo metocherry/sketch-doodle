fbc3522dceca0696776c199cd7f0b1ffce750ef5
jinseok.choi <metocherry@gmail.com>
2019-04-02 17:29:19 +0900

    Remove clipboard

--- packages/clipboard/__tests__/clipboard.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const clipboard = require('..');
-
-describe('clipboard', () => {
-    it('needs tests');
-});
--- packages/clipboard/lib/clipboard.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = clipboard;
-
-function clipboard() {
-    // TODO
-}
